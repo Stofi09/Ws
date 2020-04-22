7b575263c25bd63a1e5f4c658b37637c31df0db5
Kris <kristof.solak@gmail.com>
2020-04-22 11:10:49 +0100

    1.0

---
+++ src/main/java/com/poker/ws/WsMark1Application.java
@@ -0,0 +1,13 @@
+package com.poker.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMark1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/poker/ws/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.poker.ws.controller;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.poker.ws.chatmassage.ChatMessage;
+
+@Controller
+public class HomeController {
+
+	@MessageMapping("/chat.register")
+	@SendTo("/topic/public")
+	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+	
+	@MessageMapping("/chat.check")
+	@SendTo("/topic/public")
+	public ChatMessage cehckMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+	
+}
